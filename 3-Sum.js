a9355968726a832a1bc890bf40bf670f30d7a152
Joe Stowers <greenchiletexan@gmail.com>
2017-01-09 23:01:40 -0700

    3-Sum returns the number of triple integers that sum to 0.

---
+++ 3-Sum.js
@@ -0,0 +1,13 @@
+// Joseph Stowers
+// Toy Problem
+// January 9, 2017
+
+// 3-Sum
+
+/*
+	Given an array of N distinct integers, how many triples sum to exactly zero?
+
+*/
+
+let array = [30, -40, -10, 40, 0, 10, 5]
+
